f7d072b60e791fb99b6e9a2af94d1f598147be90
Leandro Santos <44986666+leandrosax10@users.noreply.github.com>
2023-10-21 10:14:44 -0300

    feat: criacao home page e drawer

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../shared/widgets/custon_drawer.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -12,9 +14,11 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          appBar: AppBar(
-        title: Text("Home Page"),
-      )),
+        appBar: AppBar(
+          title: Text("Home Page"),
+        ),
+        drawer: CustonDrawer(),
+      ),
     );
   }
 }
---
+++ lib/shared/widgets/custon_drawer.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:lista_de_contatos/pages/home/home_page.dart';
+
+class CustonDrawer extends StatelessWidget {
+  const CustonDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(children: [
+        InkWell(
+          child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+              width: double.infinity,
+              child: const Row(
+                children: [
+                  FaIcon(
+                    FontAwesomeIcons.userSecret,
+                    color: Color.fromARGB(255, 25, 3, 29),
+                    size: 20,
+                  ),
+                  SizedBox(width: 5),
+                  Text("Dados cadastrais"),
+                ],
+              )),
+          onTap: () {
+            /*  Navigator.pop(context);
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        const DadosCadastraisHivePageState() )); */
+          },
+        ),
+        const Divider(),
+        const SizedBox(height: 10),
+        InkWell(
+          child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+              width: double.infinity,
+              child: const Row(
+                children: [
+                  Icon(Icons.exit_to_app),
+                  SizedBox(width: 5),
+                  Text("Sair"),
+                ],
+              )),
+          onTap: () {
+            showDialog(
+                context: context,
+                builder: (BuildContext bc) {
+                  return AlertDialog(
+                    alignment: Alignment.centerLeft,
+                    elevation: 8,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    title: const Text(
+                      "Meu App",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    content: const Wrap(
+                      children: [
+                        Text("Você sairá do aplicativo!"),
+                        Text("Deseja realmente sair do aplicativo?"),
+                      ],
+                    ),
+                    actions: [
+                      TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text("Não")),
+                      TextButton(
+                          onPressed: () {
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const HomePage()));
+                          },
+                          child: const Text("Sim")),
+                    ],
+                  );
+                });
+          },
+        ),
+      ]),
+    );
+  }
+}
